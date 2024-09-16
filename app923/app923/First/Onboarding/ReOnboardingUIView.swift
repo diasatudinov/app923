0a64a0793471655069a66f85c4650035f263f026
Dias <dias.atudinov@gmail.com>
2024-09-16 13:53:58 +0500

    loading view and reviewer onboarding

--- app923/app923/First/Onboarding/ReOnboardingUIView.swift
+++ app923/app923/First/Onboarding/ReOnboardingUIView.swift
@@ -8,8 +8,115 @@
 import SwiftUI
 
 struct ReOnboardingUIView: View {
+    @State private var pageNum: Int = 1
+    @State private var showSheet = false
+    @AppStorage("signedUP") var signedUP: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if !signedUP {
+            ZStack {
+                Color.mainBg
+                    .ignoresSafeArea()
+                VStack(spacing: 0) {
+                    
+                    switch pageNum {
+                    case 1: Image("app923Screen1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 450)
+                            .padding(.top, 50)
+                            
+                    case 2: Image("app923Screen2")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 450)
+                            .padding(.top, 50)
+                    default:
+                        Image("appScreen3")
+                            .resizable()
+                            .frame(height: 549)
+                            .ignoresSafeArea()
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.secondBg)
+                        
+                        VStack {
+                            switch pageNum {
+                            case 1:
+                                VStack(spacing: 12) {
+                                    Text("Track Your Spending")
+                                        .font(.title)
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        .multilineTextAlignment(.center)
+                                    Text("Keep an eye on your beauty expenses and stay within your budget")
+                                        .font(.system(size: 15))
+                                        .multilineTextAlignment(.center)
+                                        .foregroundColor(.gray.opacity(0.7))
+                                }.frame(height: 100).padding(.bottom, 10).padding(.horizontal, 30).foregroundColor(.white).padding(.top, 24)
+                            case 2:
+                                VStack(spacing: 12) {
+                                    Text("Organize Your Collection")
+                                        .font(.title)
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        .multilineTextAlignment(.center)
+                                    Text("Manage your beauty products and plan future purchases")
+                                        .font(.system(size: 15))
+                                        .multilineTextAlignment(.center)
+                                        .foregroundColor(.gray.opacity(0.7))
+                                }.frame(height: 100).padding(.bottom, 10).padding(.horizontal, 30).foregroundColor(.white).padding(.top, 24)
+                            default:
+                                Text("Save information about \nyour favorite routes")
+                                    .font(.title)
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                    .frame(height: 70)
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(.white)
+                                    .padding(.bottom, 10)
+                                
+                            }
+                            Spacer()
+                            Button {
+                                if pageNum < 2 {
+                                    pageNum += 1
+                                } else {
+                                    signedUP = true
+                                }
+                            } label: {
+                                Text("Next")
+                                    .foregroundColor(Color.white)
+                                    .font(.system(size: 15))
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                            }.frame(height: 50).background(Color.blueBtn)
+                                .cornerRadius(16).padding(.horizontal)
+                            
+                            HStack(spacing: 8) {
+                                Circle()
+                                    .fill(pageNum == 1 ? Color.blueBtn : Color.blueBtn.opacity(0.5))
+                                    .frame(width: 8, height: 8)
+                                    .cornerRadius(19)
+                                
+                                Circle()
+                                    .fill(pageNum == 2 ? Color.blueBtn : Color.blueBtn.opacity(0.5))
+                                    .frame(width: 8, height: 8)
+                                    .cornerRadius(19)
+                                
+                            }
+                            .padding()
+                            Spacer()
+                        }
+                        
+                    }
+                    
+                    
+                }.edgesIgnoringSafeArea(.bottom)
+            }
+            
+        } else {
+           // TabUIView()
+        }
     }
 }
 
