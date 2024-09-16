000860810e5231e0edbbe5998c08c72b8b6b44fa
Dias <dias.atudinov@gmail.com>
2024-09-16 13:53:49 +0500

    loading view and reviewer onboarding new files

---
+++ app923/app923/app923App.swift
@@ -0,0 +1,17 @@
+//
+//  app923App.swift
+//  app923
+//
+//  Created by Dias Atudinov on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct app923App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
