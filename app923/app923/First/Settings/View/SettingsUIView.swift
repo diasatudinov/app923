13af49ec11b91b6f4d114aa0273d165a0726e365
Dias <dias.atudinov@gmail.com>
2024-09-17 17:14:59 +0500

    Settings View Model

--- app923/app923/First/Settings/View/SettingsUIView.swift
+++ app923/app923/First/Settings/View/SettingsUIView.swift
@@ -8,11 +8,81 @@
 import SwiftUI
 
 struct SettingsUIView: View {
+    @ObservedObject var viewModel: SettingsViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.mainBg.ignoresSafeArea()
+            
+            VStack(spacing: 12) {
+                
+                HStack {
+                    Text("Settings")
+                        .font(.system(size: 34, weight: .bold))
+                        .foregroundColor(.white)
+                    Spacer()
+                }.padding(.bottom)
+                
+                
+                Button {
+                    viewModel.openUsagePolicy()
+                } label: {
+                    HStack(alignment: .top) {
+                        Text("Usage Policy")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                        Spacer()
+                        
+                        Image(systemName: "doc.text.magnifyingglass")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 38))
+                    }.padding().overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.blue, lineWidth: 2)
+                    )
+                }
+                
+                Button {
+                    viewModel.rateApp()
+                } label: {
+                    HStack(alignment: .top) {
+                        Text("Rate our app")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                        Spacer()
+                        
+                        Image(systemName: "star.square")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 38))
+                    }.padding().padding(.vertical,4).overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.blue, lineWidth: 2)
+                    )
+                }
+                
+                Button {
+                    viewModel.shareApp()
+                } label: {
+                    HStack(alignment: .top) {
+                        Text("Share our app")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .bold))
+                        Spacer()
+                        
+                        Image(systemName: "square.and.arrow.up")
+                            .foregroundColor(.blue)
+                            .font(.system(size: 38))
+                    }.padding().overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.blue, lineWidth: 2)
+                    )
+                }
+                
+                Spacer()
+            }.padding()
+        }
     }
 }
 
 #Preview {
-    SettingsUIView()
+    SettingsUIView(viewModel: SettingsViewModel())
 }
--- app923/app923/First/Settings/ViewModel/SettingsViewModel.swift
+++ app923/app923/First/Settings/ViewModel/SettingsViewModel.swift
@@ -5,4 +5,30 @@
 //  Created by Dias Atudinov on 17.09.2024.
 //
 
-import Foundation
+import SwiftUI
+import StoreKit
+
+class SettingsViewModel: ObservableObject {
+    
+    
+    func shareApp() {
+        guard let url = URL(string: "https://apps.apple.com/app/golfstat-win-guru/id6670453682") else { return }
+        let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            rootViewController.present(activityVC, animated: true, completion: nil)
+        }
+    }
+    
+    func rateApp() {
+        SKStoreReviewController.requestReview()
+    }
+    
+    func openUsagePolicy() {
+        guard let url = URL(string: "https://www.termsfeed.com/live/61db5db7-6501-43e9-8422-5c4aa3e6ddd0") else { return }
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+}
